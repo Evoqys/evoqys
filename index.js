7a33c9b7b6d38c1bb8d0958cd94b0f75c3697d17
Prince <prince17.ps6@gmail.com>
2019-09-29 14:06:23 +0530

    navbar color transition

--- index.js
+++ index.js
@@ -16,11 +16,29 @@ $(document).ready(function(){
             $("#mynav").css("background" , "transparent");
         }
 
-        else if (scroll> 50 && scroll < 666) {
-            $("#mynav").css("background" , "blue");
+        else if (scroll>= 50 && scroll < 666) {
+            $("#mynav").css("background" , "linear-gradient(#ab4be6 0%, #ffad2b 100%)");
+        }
+        else if (scroll>= 666 && scroll < 1416) {
+            $("#mynav").css("background" , "#46A086");
+        }
+        else if (scroll>= 1416 && scroll < 2166) {
+            $("#mynav").css("background" , "#ab4be6");
+        }
+        else if (scroll>= 2166 && scroll < 2916) {
+            $("#mynav").css("background" , "#3798DB");
+        }
+        else if (scroll>= 2916 && scroll < 3666) {
+            $("#mynav").css("background" , "#46A086");
+        }
+        else if (scroll>= 3666 && scroll < 4416) {
+            $("#mynav").css("background" , "#3798DB");
+        }
+        else if (scroll>= 4416 && scroll < 5166) {
+            $("#mynav").css("background" , "#2C3E50");
         }
         else {
-            $("#mynav").css("background" , "red");
+            $("#mynav").css("background" , "#E50000");
         }
     })
 })
